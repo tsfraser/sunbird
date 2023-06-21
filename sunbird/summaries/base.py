f40404cfbcb6f80164a3b60edfe3e0e4229b1519
Tristan Sohrab Fraser <tsfraser@login13.chn>
2023-06-21 09:22:24 -0700

    commented return_errors

--- sunbird/summaries/base.py
+++ sunbird/summaries/base.py
@@ -196,7 +196,7 @@ class BaseSummary:
         slice_filters: Optional[Dict] = None,
         use_xarray: bool = False,
         batch: bool = False,
-        return_errors: bool = True,
+        #return_errors: bool = True,
     ) -> Union[np.array, xarray.DataArray]:
         """Forward pass of the neural network
 
@@ -242,8 +242,8 @@ class BaseSummary:
             if return_errors:
                 return prediction, errors
             return prediction
-        if return_errors:
-            return prediction.reshape(-1), errors.reshape(-1)
+        #if return_errors:
+            #return prediction.reshape(-1), errors.reshape(-1)
         return prediction.reshape(-1)
 
     def find_index(self, arr, num, mode="below"):
@@ -368,7 +368,7 @@ class BaseSummary:
         select_filters: Optional[Dict] = None,
         slice_filters: Optional[Dict] = None,
         use_xarray: bool = False,
-        return_errors: bool = True,
+        #return_errors: bool = True,
     ) -> Union[np.array, xarray.DataArray]:
         inputs = np.array([param_dict[k] for k in self.input_names]).reshape(1, -1)
         return self.get_for_sample(
@@ -376,7 +376,7 @@ class BaseSummary:
             select_filters=select_filters,
             slice_filters=slice_filters,
             use_xarray=use_xarray,
-            return_errors=return_errors,
+            #return_errors=return_errors,
         )
 
     def get_for_sample(self, inputs, select_filters, slice_filters, use_xarray, return_errors):
@@ -385,7 +385,7 @@ class BaseSummary:
             select_filters=select_filters,
             slice_filters=slice_filters,
             use_xarray=use_xarray,
-            return_errors=return_errors,
+            #return_errors=return_errors,
         )
 
     def get_for_batch(
@@ -394,7 +394,7 @@ class BaseSummary:
         select_filters,
         slice_filters,
         use_xarray=False,
-        return_errors=True,
+        #return_errors=True,
     ):
         inputs = torch.tensor(
             np.array([param_dict[k] for k in self.input_names]),
@@ -405,7 +405,7 @@ class BaseSummary:
             select_filters=select_filters,
             slice_filters=slice_filters,
             use_xarray=use_xarray,
-            return_errors=return_errors,
+            #return_errors=return_errors,
         )
 
     def get_for_batch_inputs(
@@ -414,7 +414,7 @@ class BaseSummary:
         select_filters=None,
         slice_filters=None,
         use_xarray=False,
-        return_errors=True,
+        #return_errors=True,
     ):
         outputs, variance = self.forward(
             inputs,
@@ -422,7 +422,7 @@ class BaseSummary:
             slice_filters=slice_filters,
             batch=True,
             use_xarray=use_xarray,
-            return_errors=return_errors,
+            #return_errors=return_errors,
         )
         if use_xarray:
             outputs = outputs.values
